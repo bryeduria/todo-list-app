461b33bda081f14bb40ea958ea3232f695819857
Bryan Eduria <eduria.bryan.96@gmail.com>
2022-01-20 17:07:30 +0800

    add task component - wip

---
+++ src/components/task.js
@@ -0,0 +1,7 @@
+const Task = (props) => {
+    return (
+        <li className='tasks-item'>{props.content}</li>
+    )
+}
+
+export default Task
--- src/components/taskList.js
+++ src/components/taskList.js
@@ -3,6 +3,7 @@ const TaskList = () => {
     return (
         <ul>
             <li className='tasks-item'>Follow me on <a href='https://github.com/bryeduria'>Github</a>.</li>
+            <li className='tasks-item'>Follow me on <a href='https://github.com/bryeduria'>Twitter</a>.</li>
         </ul>
     )
 }
