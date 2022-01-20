ab547f642e3173da712b01ed3878d5a0028b93b5
Bryan Eduria <eduria.bryan.96@gmail.com>
2022-01-20 16:30:35 +0800

    added an inline style for paragraph

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css'
 
 const App  = () =>  {
 
+    const descriptionStyle = {
+      paddingBottom: "20px"
+    }
+
     return (
         <div className='app'>
             <header className='app-header'>
 
                 <h1>Hello World!</h1>
-                <p>The most simple and amazing todo list for React app.</p>
+                <p style={descriptionStyle} className='description'>The most simple and amazing todo list for React app.</p>
+
+                <ul>
+                    <li className='tasks-item'>Follow me on <a href='https://github.com/bryeduria'>Github</a>.</li>
+                </ul>
 
             </header>
         </div>
